b608d66ddbbac10e3fcdc5b02ea2f212b4e36669
bibingeo <31274382+bibingeo@users.noreply.github.com>
2020-04-27 15:31:50 +0530

    Delete add.py

--- add.py
+++
@@ -1,9 +0,0 @@
-# def add(a,b):
-#     result=a+b
-#     return result
-
-#Sum of two numbers
-a=int(input("Enter first number:"))
-b=int(input("Enter second number"))
-sum=a+b
-print("Sum is ",sum)
