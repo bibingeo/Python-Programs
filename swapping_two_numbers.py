07ae9c077a4b4c497468e9e48cb6db1303f97e26
bibingeo <31274382+bibingeo@users.noreply.github.com>
2020-04-27 15:46:55 +0530

    Add files via upload

---
+++ swapping_two_numbers.py
@@ -0,0 +1,9 @@
+#swapping of two values or exchange the value of two variables
+number1= input("Enter value of Number1: ")
+number2= input("Enter value of Number2: ")
+temp=number1
+number1 =number2
+number2= temp
+print("After swapping:")
+print("First Value =", number1)
+print("Second Value=", number2)
