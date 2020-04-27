13900baf0b58c3044dd0207512d38bafc607b806
bibingeo <31274382+bibingeo@users.noreply.github.com>
2020-04-27 15:34:36 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,9 @@
+# def add(a,b):
+#     result=a+b
+#     return result
+
+#Sum of two numbers
+number1=int(input("Enter first number:"))
+number2=int(input("Enter second number"))
+sum=number1+number2
+print("Sum is ",sum)
---
+++ simpleinterest.py
@@ -0,0 +1,6 @@
+#Simple interest
+P=int(input("enter the value of P:"))
+N=int(input("enter the value of N:"))
+R=int(input("enter the value of R:"))
+SI=P*N*R/100
+print("Simple interest  is", SI)
